d17940a3008b0874e2c89ce95f670c65a4743a72
BakirK <bakir.karovic@gmail.com>
2019-05-27 10:18:10 +0200

    Implementirano citanje iz tekstualne datoteke te parsiranje u niz gradova

--- src/ba/unsa/rs/tutorijal10/Grad.java
+++ src/ba/unsa/rs/tutorijal10/Grad.java
@@ -1,15 +1,18 @@
 package ba.unsa.rs.tutorijal10;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 
 public class Grad implements Serializable {
 
     private String naziv;
-    private int brojStanovnika;
-    private double[] temperature = new double[1000];
+    private Integer brojStanovnika;
+    private ArrayList<Double> temperature;
 
-    public Grad() {
-
+    public Grad(String naziv, Integer brojStanovnika, ArrayList<Double> temperature) {
+        this.naziv = naziv;
+        this.brojStanovnika = brojStanovnika;
+        this.temperature = temperature;
     }
 
     public String getNaziv() {
@@ -20,19 +23,28 @@ public class Grad implements Serializable {
         this.naziv = naziv;
     }
 
-    public int getBrojStanovnika() {
+    public Integer getBrojStanovnika() {
         return brojStanovnika;
     }
 
-    public void setBrojStanovnika(int brojStanovnika) {
+    public void setBrojStanovnika(Integer brojStanovnika) {
         this.brojStanovnika = brojStanovnika;
     }
 
-    public double[] getTemperature() {
+    public ArrayList<Double> getTemperature() {
         return temperature;
     }
 
-    public void setTemperature(double[] temperature) {
+    public void setTemperature(ArrayList<Double> temperature) {
         this.temperature = temperature;
+    }
+
+    @Override
+    public String toString() {
+        String s = naziv + ", " + brojStanovnika;
+        for (double d: temperature) {
+            s += ", " + d;
+        }
+        return s;
     }
 }
--- src/ba/unsa/rs/tutorijal10/Tutorijal.java
+++ src/ba/unsa/rs/tutorijal10/Tutorijal.java
@@ -1,27 +1,62 @@
 package ba.unsa.rs.tutorijal10;
 
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Tutorijal {
 
     public static void main(String[] args) {
-	// write your code here
+        ArrayList<Grad> gradovi = ucitajGradove();
+        for (Grad grad: gradovi) {
+            System.out.println(grad.toString());
+        }
+
+        // write your code here
     }
 
 
 
     public static ArrayList<Grad> ucitajGradove() {
         ArrayList<Grad> gradovi = new ArrayList<>();
-        Scanner ulaz;
+        BufferedReader ulaz;
         try {
-            ulaz = new Scanner(new FileReader("ulaz.txt"));
+            ulaz = new BufferedReader(new FileReader("mjerenja.txt"));
+
+            String line;
+            //ulaz.hasNext();
+            while((line = ulaz.readLine()) != null) {
+
+                short i = -1;
+                String naziv = new String();
+                ArrayList<Double> temperature = new ArrayList<>();
+                //double[] temperature = new double[10];
+                for(String word: line.split(",")) {
+                    if (i == -1) {
+                        naziv = word;
+
+                        i++;
+                    } else {
+                        //System.out.println(word);
+                        //temperature[i] = Double.parseDouble(word);
+                        temperature.add(Double.parseDouble(word));
+                        i++;
+                    }
+                    if ( i == 10) {
+                        break;
+                    }
+                }
+
+                gradovi.add(new Grad(naziv, null, temperature));
+            }
         } catch (FileNotFoundException e) {
             System.out.println("Datoteka ulaz.txt ne postoji ili se ne može otvoriti");
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-
         return gradovi;
     }
 }
