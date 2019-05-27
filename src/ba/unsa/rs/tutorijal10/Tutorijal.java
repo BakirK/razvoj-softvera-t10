b779dfab912eb9dab2f0032cf059f9a1409aa37f
BakirK <bakir.karovic@gmail.com>
2019-05-27 09:25:51 +0200

    Dodan implements Serializable kod svih klasa zbog xml files

--- src/ba/unsa/rs/tutorijal10/Tutorijal.java
+++ src/ba/unsa/rs/tutorijal10/Tutorijal.java
@@ -1,8 +1,27 @@
 package ba.unsa.rs.tutorijal10;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Tutorijal {
 
     public static void main(String[] args) {
 	// write your code here
+    }
+
+
+
+    public static ArrayList<Grad> ucitajGradove() {
+        ArrayList<Grad> gradovi = new ArrayList<>();
+        Scanner ulaz;
+        try {
+            ulaz = new Scanner(new FileReader("ulaz.txt"));
+        } catch (FileNotFoundException e) {
+            System.out.println("Datoteka ulaz.txt ne postoji ili se ne može otvoriti");
+        }
+
+        return gradovi;
     }
 }
