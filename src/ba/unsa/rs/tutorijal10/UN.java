a34488d14e3772a4358ac691cc09eae1d2cc70c0
BakirK <bakir.karovic@gmail.com>
2019-05-27 09:19:56 +0200

    Initial commit

---
+++ src/ba/unsa/rs/tutorijal10/UN.java
@@ -0,0 +1,17 @@
+package ba.unsa.rs.tutorijal10;
+
+import java.util.ArrayList;
+
+public class UN {
+    private ArrayList<Drzava> drzave;
+
+    public ArrayList<Drzava> getDrzave() {
+        return drzave;
+    }
+
+    public void setDrzave(ArrayList<Drzava> drzave) {
+        this.drzave = drzave;
+    }
+
+    public UN() {}
+}
